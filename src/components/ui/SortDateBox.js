72c1bc3d07b83527a9b6e1d054feb9d5a80e137d
tal8374 <talivanov@gmail.com>
2018-11-09 16:14:15 +0200

    Styling sort by date

--- src/components/ui/SortDateBox.js
+++ src/components/ui/SortDateBox.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import enhance from '../hoc/wrapInputBox';
-import classes from "./SortDateBox.css";
 
 function SortDateBox(props) {
     const {setFromDate, setToDate} = props;
 
     return (
-        <div className={classes.row}>
-            <p className={classes.rightSpace}>From</p>
-            <input className={classes.rightSpace} onChange={e => setFromDate(e.target.value)} type="date" />
-            <p className={classes.rightSpace}>To - </p>
-            <input className={classes.rightSpace} onChange={e => setToDate(e.target.value)} type="date" />
+        <div className="row container">
+            <p className="rightSpace">From</p>
+            <input className="rightSpace" onChange={e => setFromDate(e.target.value)} type="date" placeholder="Search"/>
+            <p className="rightSpace">To</p>
+            <input className="rightSpace" onChange={e => setToDate(e.target.value)} type="date" placeholder="Search"/>
         </div>
     );
 }
